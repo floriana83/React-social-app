5e95500a7700cf8692994cd10b3d21ec0c6ae8bd
floriana83 <floriana83@users.noreply.github.com>
2022-01-12 15:02:10 +0000

    deploy: 5dded63a475d66211ccc94cd775330f702f333a3

--- static/js/messages.45bde9cb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-social-app"]=this["webpackJsonpreact-social-app"]||[]).push([[4],{45:function(s,a,c){"use strict";c.r(a);var e=c(1);a.default=function(){return Object(e.jsx)("h1",{children:"Messagesssss"})}}}]);
-//# sourceMappingURL=messages.45bde9cb.chunk.js.map
